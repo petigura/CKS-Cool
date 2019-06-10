27a671ecf32d656d5600d38215be573bf3c58069
Erik Petigura <petigura@caltech.edu>
2019-06-09 22:07:53 -0700

    isoclassify complete

--- bin/create_tot.sh
+++ bin/create_tot.sh
@@ -1,7 +1,7 @@
 sources=(cks1 smsyn smemp)
 for source in "${sources[@]}"
 do
-    isoclassify batch direct data/isoclassify-${source}-direct.csv -o isoclassify/${source}/direct/ > isoclassify-${source}-direct.tot
-    isoclassify batch grid data/isoclassify-${source}-grid-parallax-yes.csv  -o isoclassify/${source}/grid-parallax-yes/ > isoclassify-${source}-grid-parallax-yes.tot
-    isoclassify batch grid data/isoclassify-${source}-grid-parallax-no.csv -o isoclassify/${source}/grid-parallax-no/ > isoclassify-${source}-grid-parallax-no.tot
+    isoclassify batch direct data/isoclassify-${source}-direct.csv -o isoclassify/${source}/direct/ --plot none > isoclassify-${source}-direct.tot
+    isoclassify batch grid data/isoclassify-${source}-grid-parallax-yes.csv  -o isoclassify/${source}/grid-parallax-yes/ --plot none > isoclassify-${source}-grid-parallax-yes.tot
+    isoclassify batch grid data/isoclassify-${source}-grid-parallax-no.csv -o isoclassify/${source}/grid-parallax-no/ --plot none > isoclassify-${source}-grid-parallax-no.tot
 done
