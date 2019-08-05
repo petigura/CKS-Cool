51d98f5061b427194a6cdb8e73b09887030b59ec
Erik Petigura <petigura@caltech.edu>
2019-08-04 17:50:32 -0700

    cleaning up code

--- ckscool/calc.py
+++ ckscool/calc.py
@@ -1,26 +1,39 @@
-import pandas as pd
-import numpy as np
 from collections import OrderedDict
 
-SEED = 0  # reproducability
-rand = np.random.RandomState(SEED)
+import numpy as np
+import pandas as pd
 
-albedo = 0.30
+SEED = 0  # reproducability
+np.random.RandomState(SEED)
 
 def update_planet_parameters(df):
-    newkeys = [
-        'gdir_prad','gdir_prad_err1','gdir_prad_err2',
-        'giso_sinc','giso_sinc_err1','giso_sinc_err2',
-        'giso_sma','giso_sma_err1','giso_sma_err2',
-    ]
+    """Update planet parameters
 
-    n = len(df)
+    Args:
+         df: DataFrame
+
+    Returns
+        pandas.DataFrame: with following columns added
+
+         -    gdir_prad
+         -    gdir_prad_err1
+         -    gdir_prad_err2
+         -    giso_prad
+         -    giso_prad_err1
+         -    giso_prad_err2
+         -    giso_sinc
+         -    giso_sinc_err1
+         -    giso_sinc_err2
+         -    giso_sma
+         -    giso_sma_err1
+         -    giso_sma_err2
+
+    """
     nsamp = 1000
-
     def sample(loc, scale):
-        loc = np.array(loc).reshape(-1,1)
-        scale = np.array(scale).reshape(-1,1)
-        return loc + scale * np.random.randn(1,nsamp)
+        loc = np.array(loc).reshape(-1, 1)
+        scale = np.array(scale).reshape(-1, 1)
+        return loc + scale * np.random.randn(1, nsamp)
 
     # stellar radius direct method
     loc = df.gdir_srad
@@ -48,29 +61,29 @@ def update_planet_parameters(df):
     empty = scale.isnull()
     scale = scale.fillna(0)
     ror_samp = sample(loc, scale)
-    ror_samp[empty,:] = np.nan # fill with null
+    ror_samp[empty, :] = np.nan # fill with null
 
     # Planet radius
     gdir_prad_samp = ror_samp * gdir_srad_samp * 109.245
     giso_prad_samp = ror_samp * giso_srad_samp * 109.245
 
     # Semi-major axis
-    period = np.array(df.koi_period).reshape(-1,1)
+    period = np.array(df.koi_period).reshape(-1, 1)
     sma_samp = (smass_samp * (period/365)**2) ** 0.33
 
     # insolation flux
     sinc_samp = (steff_samp/5778)**4 * (giso_srad_samp/sma_samp)**2
 
-    df['gdir_prad'] = np.median(gdir_prad_samp,axis=1)
-    df['gdir_prad_err1'] = np.std(gdir_prad_samp,axis=1)
+    df['gdir_prad'] = np.median(gdir_prad_samp, axis=1)
+    df['gdir_prad_err1'] = np.std(gdir_prad_samp, axis=1)
     df['gdir_prad_err2'] = -1.0 * df['gdir_prad_err1']
     df['giso_prad'] = np.median(giso_prad_samp)
     df['giso_prad_err1'] = np.std(giso_prad_samp)
     df['giso_prad_err2'] = -1.0 * df['giso_prad_err1']
-    df['giso_sinc'] = np.median(sinc_samp,axis=1)
-    df['giso_sinc_err1'] = np.std(sinc_samp,axis=1)
+    df['giso_sinc'] = np.median(sinc_samp, axis=1)
+    df['giso_sinc_err1'] = np.std(sinc_samp, axis=1)
     df['giso_sinc_err2'] = -1.0 * df['giso_sinc_err1']
-    df['giso_sma'] = np.median(sma_samp,axis=1)
-    df['giso_sma_err1'] = np.std(sma_samp,axis=1)
+    df['giso_sma'] = np.median(sma_samp, axis=1)
+    df['giso_sma_err1'] = np.std(sma_samp, axis=1)
     df['giso_sma_err2'] = -1.0 * df['giso_sma_err1']
     return df
