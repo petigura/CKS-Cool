7b01f921edf297c57d000e27f781e464cf8f1f7b
Erik Petigura <petigura@caltech.edu>
2019-08-06 17:38:13 -0700

    both planet and field smass are m17_smass

--- ckscool/occur.py
+++ ckscool/occur.py
@@ -179,7 +179,7 @@ def load_occur(limits, debug=False):
 
     plnt = ckscool.io.load_table('planets-cuts2+iso')
     plnt = plnt[~plnt.isany]
-    namemap = {'gdir_prad':'prad','koi_period':'per','giso_smass':'smass'}
+    namemap = {'gdir_prad':'prad','koi_period':'per','m17_smass':'smass'}
     plnt = plnt.rename(columns=namemap)
 
     if limits.has_key('smass1'):
