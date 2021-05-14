5b1a1e1cf5d22113590ff797b4328a6296d4f614
Erik Petigura <petigura@astro.ucla.edu>
2021-05-13 21:21:51 -0700

    removed val_stat

--- ckscool/value.py
+++ ckscool/value.py
@@ -7,114 +7,6 @@ from time import gmtime, strftime
 from astropy.time import Time
 import pandas as pd
 
-
-def val_stat(return_dict=False):
-    """
-    Statistics of sample
-    """
-    d = OrderedDict()
-
-
-    cand = ckscool.io.load_table('planets-cuts1',cache=2)
-    star = cand.groupby('id_kic',as_index=False).nth(0)
-    field = ckscool.io.load_table('field-cuts',cache=2)
-
-    d['n-koi'] = "{}".format( len(cand) )
-    d['n-star'] = "{}".format( len(star) )
-
-    d['n-star-field'] = "{}".format( len(field) )
-    d['n-star-field-pass0'] = "{}".format( len(field[~field.isany]) )
-
-    df = ckscool.io.load_table('planets-cuts1')
-    df = ckscool.cuts.occur.add_cuts(df, field.cuttypes, 'field')
-    d['n-cand'] = "{}".format( len(df) )
-
-    cut = df[~df.isany]
-    d['n-cand-pass0'] = "{}".format( len(cut ) )
-    d['n-star-pass0'] = "{}".format( len(cut.id_koi.drop_duplicates()) )
-
-    df = ckscool.io.load_table('planets-cuts1')
-    cuttypes1 = df.cuttypes
-    cand = df[~df.isany]
-    d['n-cand-pass1'] = "{}".format( len(cand) )
-    star1 = cand.groupby('id_koi',as_index=False).nth(0)
-    d['n-star-pass1'] = "{}".format( len(star1) )
-
-    df = ckscool.io.load_table('planets-cuts2+iso')
-    star2 = df.groupby('id_koi',as_index=False).nth(0)
-    m = pd.merge(star1,star2[['id_koi','cks_sprov']],how='left')
-    m['cks_sprov'].fillna('none',inplace=True)
-    s = m.groupby('cks_sprov').size()
-    for sprov, count in s.iteritems():
-        d['n-star-pass1-{}'.format(sprov,count)] = count
-
-    d['n-star-pass1-newobs'] = s['smemp'] + s['smsyn']
-    d['n-star-pass1-lt4700'] = len(m.query('ber18_steff < 4700'))
-    
-    df[df[["is"+c for c in cuttypes1]].sum(axis=1)==0]
-    d['sparallax-med'] = "{:.1f}".format(star.eval('gaia2_sparallax').median())
-    d['sparallax-ferr-med'] = "{:.1f}".format(star.eval('gaia2_sparallax_err / gaia2_sparallax').median() * 100)
-    d['sdistance-med'] = "{:.0f}".format(star.eval('1000 / gaia2_sparallax').median())
-
-    d['kmag-med'] = "{:.2f}".format(star.m17_kmag.median())
-    d['kmag-err-med'] = "{:.2f}".format(star.m17_kmag_err.median())
-    d['kepmag-med'] = "{:.1f}".format(star.m17_kepmag.median())
-    d['kepmag-err-med'] = "{:.1f}".format(star.m17_kepmag.median())
-    
-    av_to_ak = (0.161+0.063)/(2.9197679+0.063) # extinction law
-
-    df = ckscool.io.load_table('planets+iso',cache=1)
-    d['av-med'] = "{:.03f}".format(df.gdir_avs.median())
-    d['av-min'] = "{:.03f}".format(df.gdir_avs.min())
-    d['av-max'] = "{:.03f}".format(df.gdir_avs.max())
-    d['ak-med'] = "{:.03f}".format(float(d['av-med']) * av_to_ak)
-    d['ak-min'] = "{:.03f}".format(float(d['av-min']) * av_to_ak)
-    d['ak-max'] = "{:.03f}".format(float(d['av-max']) * av_to_ak)
-
-
-    ferr = df.eval('0.5*(gdir_srad_err1 - gdir_srad_err2) / gdir_srad')
-    d['srad-med'] = "{:.1f}".format(df.gdir_srad.median())
-    d['srad-ferr-med'] = "{:.1f}".format(100*ferr.median())
-
-    '''
-    df = ckscool.io.load_table('ckscool-stars-cuts',cache=0)
-    cuttypes = df.cuttypes
-    df = df.groupby('id_koi').nth(0)
-
-    for cut in cuttypes:
-        d['cut-nstars-'+cut] = df['is'+cut].sum()
-
-    d['cut-nstars-any'] = df['isany'].sum()
-
-    obs = ckscool.observe.ObserveNIRC2()
-    obs.label_observed()
-
-    d['nirc2-k18-observed'] = obs.sample['k18_observed'].sum()
-    d['nirc2-f17-observed'] = obs.sample['f17_observed'].sum()
-    d['nirc2-p18-observed'] = obs.sample['p18_observed'].sum()
-    d['nirc2-tot-observed'] = obs.sample['isobserved'].sum()
-
-
-    df = ckscool.io.load_table('cksgaia-planets')
-    df = df.groupby('id_koi',as_index=False).nth(0)
-    d['cks1-sdistance-med'] = "{:.0f}".format(df.query('kic_kepmag < 14.2').eval('1000 / gaia2_sparallax').median())
-
-    cd = ckscool.cuts.cd
-    for key in cd.keys():
-        d[key] = cd[key]
-    
-
-    '''
-    lines = []
-    for k, v in d.iteritems():
-        line = r"{{{}}}{{{}}}".format(k,v)
-        lines.append(line)
-
-    if return_dict:
-        return d
-
-    return lines
-
 def val_sample(return_dict=False):
     d = OrderedDict()
 
@@ -381,35 +273,19 @@ def val_fit(return_dict=False):
 
 def val_grad(return_dict=False):
     d = OrderedDict()
-
-    # gradient slope
-    smass = [0.5,0.7,1.0,1.4]
-    for i in range(3):
-        smass1, smass2 = smass[i],smass[i+1]
-        key = 'grad-per-prad_smass={0}-{1}'.format(
-            smass1, smass2
-        )            
-        grad = ckscool.io.load_object(key,cache=1)
-        m = grad.grad_chain[:,0]
-        Rp_10 = 10**grad.grad_chain[:,1]
+    smass = ['0.5-0.7','0.7-1.0','1.0-1.4','0.5-1.4']
+    outdir = 'gapfitting-comparison/gradient-column/'
+    for _smass in smass:
+        fn = '{}/grad-per-prad-det_smass={}.csv'.format(outdir, _smass)
+        key = 'per-prad-det_smass={}'.format(_smass)
+        df = pd.read_csv(fn)
+        df['Rp10'] = 10**df.y0
+        q = df.quantile([0.14,0.5,0.86])
+        q.loc['up'] = q.loc[0.86] - q.loc[0.50]
+        q.loc['lo'] = q.loc[0.14] - q.loc[0.50]
+        d[key+'-Rp10'] = "{:.2f}^{{ +{:.2f} }}_{{ {:.2f} }}".format(*q.loc[[0.5,'up','lo'],'Rp10'].tolist())
+        d[key+'-m'] = "{:.2f}^{{ +{:.2f} }}_{{ {:.2f} }}".format(*q.loc[[0.5,'up','lo'],'m'].tolist())
         
-        d[key+'-m'] = "{:.2f} \pm {:.2f}".format(m.mean(),m.std())
-        d[key+'-Rp10'] = "{:.2f} \pm {:.2f}".format(Rp_10.mean(),Rp_10.std())
-
-
-    # gradient slope
-    smass = [0.5,0.7,1.0,1.4]
-    for i in range(3):
-        smass1, smass2 = smass[i],smass[i+1]
-        key = 'grad-sinc-prad_smass={0}-{1}'.format(
-            smass1, smass2
-        )            
-        grad = ckscool.io.load_object(key,cache=1)
-        m = grad.grad_chain[:,0]
-        Rp_100 = 10**grad.grad_chain[:,1]
-        
-        d[key+'-m'] = "{:.2f} \pm {:.2f}".format(m.mean(),m.std())
-        d[key+'-Rp100'] = "{:.2f} \pm {:.2f}".format(Rp_100.mean(),Rp_100.std())
         
     lines = []
     for k, v in d.iteritems():
